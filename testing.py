82c13606f9d7c084cef9d08d54d4209804d25da0
jackpalaia <jack@jackpalaia.com>
2020-06-09 16:46:08 -0400

    visibility of variables

--- testing.py
+++ testing.py
@@ -1,4 +1,9 @@
-a = 'first'
-b = 'second'
-c = 'third'
-print(' '.join([a,b,c]))
+def length(*t, degree=2):
+  '''computes the length of the vector given as parameter. By default,
+  the Euclidian distance (degree == 2)'''
+  s = 0
+  for x in t:
+    s += abs(x)**degree
+  return s**(1/degree)
+print(length(-4, 3))
+print(length(-4, 3, degree=3))
