80ddd4cf4f0e846a9eaa9e57196d35cd6b445131
jackpalaia <jack@jackpalaia.com>
2020-06-09 20:01:08 -0400

    sorting sequences

--- testing.py
+++ testing.py
@@ -1,9 +1,3 @@
-def length(*t, degree=2):
-  '''computes the length of the vector given as parameter. By default,
-  the Euclidian distance (degree == 2)'''
-  s = 0
-  for x in t:
-    s += abs(x)**degree
-  return s**(1/degree)
-print(length(-4, 3))
-print(length(-4, 3, degree=3))
+myList = [1,2,3,4]
+myList[1:3] = [5]
+print(myList)
